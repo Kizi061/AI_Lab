5e1e087c89537e5b59564e11d6f2b81e6344c34a
aayasha palikhe <aayasha.palikhe@gmail.com>
2025-04-29 08:17:20 -0500

    First panda

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+dataset = pd.read_csv('Walmart.csv')
+
+print("Dataset Loaded")
+# print the csv file
+print(dataset.describe) 
+
+columns = ["Weekly_Sales","Holiday_Flag","Temperature","Fuel_Price","CPI","Unemployment"]
+
+for column in columns:
+    print(f"column: {column}")
+    print(f"Mean: {dataset[column].mean()}") 
+    print(f"Median: {dataset[column].mean()}") 
+    print(f"Mode: {dataset[column].mean()}") 
