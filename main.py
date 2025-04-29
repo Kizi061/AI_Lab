850ec41e255a8eaa64a01a956dbcd6c10c7f2005
aayasha palikhe <aayasha.palikhe@gmail.com>
2025-04-29 10:06:14 -0500

    Add standard deviation

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ print(dataset.describe)
 columns = ["Weekly_Sales","Holiday_Flag","Temperature","Fuel_Price","CPI","Unemployment"]
 
 for column in columns:
-    print(f"column: {column}")
+    print(f"--------------column: {column}------------")
     print(f"Mean: {dataset[column].mean()}") 
     print(f"Median: {dataset[column].mean()}") 
     print(f"Mode: {dataset[column].mean()}") 
+    print(f"Standard Deviation: {dataset[column].std()}") 
